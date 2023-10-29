0869791b60b7b17ebf2c536935b506286e7ee709
Ash2901 <ashishashu2901@gmail.com>
2023-10-29 17:28:40 +0530

    chapter 4 commit

--- components/menu.js
+++ components/menu.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import Menu_1 from '../images/menu-1.jpg';
+// import Menu_1 from '../images/menu-1.jpg';
 
-export default Menu = () => {
+export default Menu = (props) => {
+    const { resData } = props; //always key will be passed here
+    const {cloudinaryImageId, name, cuisines, avgRating, costForTwo, slaString} = resData?.data;
     return (
         <div>
             <div className="res-card">
-                <img src={Menu_1} alt = "first item"></img>
-                <h2>Bakingo</h2>
-                <h4>Bakery, Desserts</h4>
-                <h4>4.3 stars</h4>
-                <h4>25 mins</h4>
+                <img src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId} alt = "first item"></img>
+                <h2>{name}</h2>
+                <h4>{cuisines.join(", ")}</h4> 
+                <h4>{avgRating} ⭐</h4>
+                {/* <h4>{resData.data.costForTwoString}</h4>  */}
+                <h4>₹{costForTwo/100} FOR TWO</h4>
+                <h4>{slaString}</h4> 
             </div>
         </div>
     );
